bec6c1409ee775531ebf44c6077cec2e14bc9692
Derrick White <derrickthewhite@gmail.com>
2026-03-17 12:15:22 -0500

    Maze Game updates

--- MazeGame/populate.js
+++ MazeGame/populate.js
@@ -2,11 +2,12 @@ var config = {
 	goblins:4,
 	keyGoblins:2,
 	swordGoblins:2,
-	trolls: 8,
+	trolls: 4,
+	fireElementals: 10,
 	lockedDoors:8,
 	weakWalls: 8,
 	chests: 8,
-	stairs: 2,
+	stairs: ["Fire Maze", "Goblin Town"],
 	fire: 4,
 	wells: 4,
 	keys: 4,
@@ -15,29 +16,108 @@ var config = {
 	swords: 4,
 	shields: 1,
 	potions: 2,
+	// more permenant:
+	itemLimit: 3
 }
 
-function randomChestItem() {
-	
-	/*
-		Potions (+1 heart)
-		Picks (let you knock down walls)
-		Buckets (let you put out fires)
-		Keys (let you open doors)
-	*/
-	var options = [
-		"Potion",
-		"Sword",
-		"Pick",
-		"Bucket",
-		"Key"
-	]
-	var itemName = options[Math.floor(Math.random()*options.length)];
-	
-	return {
-		name: itemName,
-		icon: "assets/"+itemName+".svg"
+const levels = {
+	"Test": {
+		goblins:0,
+		keyGoblins:0,
+		swordGoblins:0,
+		trolls: 0,
+		fireElementals: 1,
+		lockedDoors:0,
+		weakWalls: 0,
+		chests: 0,
+		stairs: ["Troll Tunnel", "Goblin Town"],
+		fire: 20,
+		webs: 20,
+		spiders: 1,
+		wells: 0,
+		keys: 0,
+		picks: 0,
+		buckets: 0,
+		swords: 0,
+		potions: 0,
+	},
+	"Spider Den": {
+		goblins:4,
+		keyGoblins:0,
+		swordGoblins:4,
+		trolls: 0,
+		fireElementals: 0,
+		spiders: 10,
+		lockedDoors:0,
+		weakWalls: 0,
+		chests: 0,
+		stairs: ["Troll Tunnel", "Fire Maze"],
+		fire: 0,
+		webs: 10,
+		wells: 0,
+		keys: 0,
+		picks: 0,
+		torches: 4,
+		buckets: 0,
+		swords: 0,
+		potions: 0,
+	},
+	"Fire Maze": {
+		goblins:3,
+		keyGoblins:0,
+		swordGoblins:3,
+		trolls: 0,
+		fireElementals: 20,
+		lockedDoors:4,
+		weakWalls: 4,
+		chests: 8,
+		stairs: ["Spider Den", "Goblin Town"],
+		fire: 20,
+		wells: 4,
+		keys: 4,
+		picks: 2,
+		buckets: 4,
+		swords: 2,
+		potions: 2,
+	},
+	"Goblin Town": {
+		goblins:4,
+		keyGoblins:4,
+		swordGoblins:4,
+		trolls: 2,
+		fireElementals: 1,
+		lockedDoors:8,
+		weakWalls: 8,
+		chests: 8,
+		stairs: ["Fire Maze", "Troll Tunnel"],
+		fire: 4,
+		wells: 4,
+		keys: 4,
+		picks: 4,
+		buckets: 4,
+		swords: 4,
+		shields: 0,
+		potions: 2,
+	},
+	"Troll Tunnel": {
+		goblins:4,
+		keyGoblins:2,
+		swordGoblins:2,
+		trolls: 12,
+		lockedDoors:4,
+		weakWalls: 4,
+		chests: 8,
+		stairs: ["Fire Maze", "Goblin Town"],
+		fire: 0,
+		wells: 0,
+		keys: 4,
+		picks: 4,
+		buckets: 4,
+		swords: 4,
+		shields: 1,
+		potions: 2,
 	}
+	
 }
 
 function placeObjects(objectTemplate, count){
@@ -45,6 +125,13 @@ function placeObjects(objectTemplate, count){
 		placeObject(objectTemplate);
 	}
 }
+function placeObjectAt(objectTemplate, coords){
+	var object = JSON.parse(JSON.stringify(objectTemplate));
+	object.x = coords.x;
+	object.y = coords.y;
+	objects.push(object)
+}
+
 function placeObject(objectTemplate) {
 	var object = JSON.parse(JSON.stringify(objectTemplate));
 	var coords = randomUnobstructedLocation();
@@ -85,7 +172,13 @@ function placeDoorInWall(doorTemplate) {
 	doors.push(door);
 }
 
-function populateMaze() {
+function populateMaze(levelName) {
+	
+	//if(levelName == undefined) levelName = "Goblin Town";
+	if(levelName == undefined) levelName = "Fire Maze";
+	//if(levelName == undefined) levelName = "Spider Den";
+	let level = levels[levelName];
+	if(!level) level = config;
 	
 	if (protagonist === undefined){
 		protagonist = {
@@ -98,13 +191,39 @@ function populateMaze() {
 			hearts: 3,
 			maxHearts: 3,
 			score:0,
-			resources: []
+			resources: [
+				{
+					name: "Torch",
+					loyalty: "object",
+					icon: "assets/Torch.svg",
+					resource: true,
+					description: "Portable Fire",
+					weaponType: "fire",
+					weaponDamage: 1
+				},
+				{
+					name: "Shield",
+					loyalty: "object",
+					icon: "assets/Shield.svg",
+					resource: true,
+					description: "In a fight, take one less damage",
+					weaponType: "physical-armor",
+					weaponDamage: 1
+				},
+				{
+					name: "Bucket",
+					loyalty: "object",
+					icon: "assets/Bucket.svg",
+					resource: true,
+					description: "allow you to put out fire when full"
+				}
+			]
 		};
 	}
 	
 	characters.push(protagonist);
 	
-	for(var i = 0; i<config.goblins; i++) {
+	for(var i = 0; i<level.goblins; i++) {
 		var coords = randomUnobstructedLocation();
 		var goblin = {
 			name: "goblin",
@@ -119,7 +238,7 @@ function populateMaze() {
 		};
 		characters.push(goblin);
 	}
-	for(var i = 0; i<config.keyGoblins; i++) {
+	for(var i = 0; i<level.keyGoblins; i++) {
 		var coords = randomUnobstructedLocation();
 		var goblin = {
 			name: "key goblin",
@@ -138,7 +257,7 @@ function populateMaze() {
 		characters.push(goblin);
 	}
 	
-	for(var i = 0; i<config.swordGoblins; i++) {
+	for(var i = 0; i<level.swordGoblins; i++) {
 		var coords = randomUnobstructedLocation();
 		var goblin = {
 			name: "sword goblin",
@@ -150,14 +269,18 @@ function populateMaze() {
 			lastY: 0,
 			doors: ["locked Door"],
 			resources: [
-				{name: "Sword"}
+				{
+					name: "Sword",
+					weaponDamage: 1,
+					weaponType: "physical"
+				}
 			],
 			hearts: 1
 		};
 		characters.push(goblin);
 	}
 	
-	for(var i = 0; i<config.trolls; i++) {
+	for(var i = 0; i<level.trolls; i++) {
 		var coords = randomUnobstructedLocation();
 		var troll = {
 			name: "troll",
@@ -168,9 +291,73 @@ function populateMaze() {
 			lastX: 0,
 			lastY: 0,
 			doors: [],
-			hearts: 2
+			maxHearts: 2,
+			liveLoyalty: "goblin",
+			liveIcon: "assets/Troll.svg",
+			regenerates: true,
+			hearts: 2,
+			description: "A Troll. An enemy that regenerates when killed"
 		};
 		characters.push(troll);
+	}
+	
+		for(var i = 0; i<level.trolls; i++) {
+		var coords = randomUnobstructedLocation();
+		var troll = {
+			name: "troll",
+			loyalty: "goblin",
+			icon: "assets/Troll.svg",
+			x: coords.x,
+			y: coords.y,
+			lastX: 0,
+			lastY: 0,
+			doors: [],
+			maxHearts: 2,
+			liveLoyalty: "goblin",
+			liveIcon: "assets/Troll.svg",
+			regenerates: true,
+			hearts: 2,
+			description: "A Troll. An enemy that regenerates when killed"
+		};
+		characters.push(troll);
+	}
+	
+	for(var i = 0; i<level.fireElementals; i++) {
+		var coords = randomUnobstructedLocation();
+		var elemental = {
+			name: "Fire Elemental",
+			loyalty: "goblin",
+			icon: "assets/FireElemental.svg",
+			x: coords.x,
+			y: coords.y,
+			lastX: 0,
+			lastY: 0,
+			doors: [],
+			hearts: 1,
+			description: "A Fire Elemental. Only damaged by water",
+			damageType: "fire",
+			vulnerable: ["water"],
+			ignores: ["physical", "fire"]
+		}
+		characters.push(elemental);
+	}
+	
+	for(var i = 0; i<level.spiders; i++) {
+		var coords = randomUnobstructedLocation();
+		var elemental = {
+			name: "Spider",
+			loyalty: "goblin",
+			icon: "assets/spider.svg",
+			x: coords.x,
+			y: coords.y,
+			lastX: 0,
+			lastY: 0,
+			doors: [],
+			hearts: 2,
+			description: "A Spider",
+			ignores: ["web"]
+		}
+		characters.push(elemental);
 	}
 	
 	doors.push({
@@ -245,7 +432,7 @@ function populateMaze() {
 	}
 	//*/
 	
-	for(var i = 0; i< config.lockedDoors; i++) {
+	for(var i = 0; i< level.lockedDoors; i++) {
 		placeDoorInWall({
 			icon: "assets/Lock.svg",
 			name: "locked Door"
@@ -253,7 +440,7 @@ function populateMaze() {
 	}
 	
 	// weak wall doors!
-	for(var i =0; i< config.weakWalls; i++) {
+	for(var i =0; i< level.weakWalls; i++) {
 		placeDoorInWall({
 			icon: "assets/WeakWall.svg",
 			name: "weak wall"
@@ -293,39 +480,54 @@ function populateMaze() {
 		*/
 	}
 	
-	for(var i =0; i<config.chests; i++) {
+	for(var i =0; i<level.chests; i++) {
 		placeObject({
 			name: "Chest",
 			loyalty: "object",
 			icon: "assets/Treasure.svg",
-			//contents: [randomChestItem(), randomChestItem()]
 			description: "A box containing the precious and shining"
 		})
 	}
 	
-	placeObjects({
-		name: "Stairs",
-		loyalty: "object",
-		icon: "assets/Stairs.svg"
-	},config.stairs);
+	for(var stairLocation of level.stairs) {
+		placeObject({
+			name: "Stairs",
+			destination: stairLocation,
+			loyalty: "object",
+			icon: "assets/Stairs.svg",
+			description: "staircase to "+ stairLocation
+		});
+	}
+
 	
 	placeObjects({
 		name: "Fire",
 		loyalty: "hazard",
 		icon: "assets/Fire.svg",
+		damageType: "fire",
 		hearts: 1,
 		description: "A raging fire. Does 1 damage. Can be put out with a bucket of water"
-	},config.fire);
+	},level.fire);
+	
+	placeObjects({
+		name: "Web",
+		loyalty: "hazard",
+		icon: "assets/Web.svg",
+		damageType: "web",
+		hearts: 1,
+		description: "A sticky web. Does 1 damage"
+	},level.webs);
 	
 	
 	for(var i = 0; i< 4; i++) {
-		placeObject({
+		let quadrants = [{x:0,y:0}, {x:1,y:0},{x:0,y:1},{x:1,y:1}];
+		placeObjectAt({
 			name: "Lever",
 			loyalty: "object",
 			icon: "assets/Lever.svg",
 			color: ["green","yellow","blue","purple"][i],
-			maze: [{x:0,y:0}, {x:1,y:0},{x:0,y:1},{x:1,y:1}][i]
-		});
+			maze: quadrants[i]
+		}, randomUnobstructedQuadrantLocation(quadrants[i].x, quadrants[i].y));
 	}
 	
 	placeObjects({
@@ -333,15 +535,17 @@ function populateMaze() {
 		loyalty: "object",
 		icon: "assets/Well.svg",
 		description: "refill your bucket here"
-	},config.wells);
+	},level.wells);
 	
 	placeObjects({
 		name: "Pick",
 		loyalty: "object",
 		icon: "assets/Pick.svg",
 		resource: true,
-		description: "allow you to break through weak walls"
-	},config.picks);
+		description: "allow you to break through weak walls",
+		weaponType: "physical",
+		weaponDamage: 1
+	},level.picks);
 	
 	placeObjects({
 		name: "Bucket",
@@ -349,7 +553,7 @@ function populateMaze() {
 		icon: "assets/Bucket.svg",
 		resource: true,
 		description: "allow you to put out fire when full"
-	},config.buckets);
+	},level.buckets);
 	
 	placeObjects({
 		name: "Key",
@@ -357,28 +561,42 @@ function populateMaze() {
 		icon: "assets/Key.svg",
 		resource: true,
 		description: "Opens doors!"
-	},config.keys);
+	},level.keys);
+	
+	placeObjects({
+		name: "Torch",
+		loyalty: "object",
+		icon: "assets/Torch.svg",
+		resource: true,
+		description: "Portable Fire",
+		weaponType: "fire",
+		weaponDamage: 1
+	},level.torches);
 	
 	placeObjects({
 		name: "Sword",
 		loyalty: "object",
 		icon: "assets/Sword.svg",
 		resource: true,
-		description: "In a fight, take one less damage"
-	},config.swords);
+		description: "In a fight, take one less damage",
+		weaponType: "physical",
+		weaponDamage: 2
+	},level.swords);
 	
 	placeObjects({
 		name: "Shield",
 		loyalty: "object",
 		icon: "assets/Shield.svg",
 		resource: true,
-		description: "In a fight, take one less damage"
-	},config.shields);
+		description: "In a fight, take one less damage",
+		weaponType: "physical-armor",
+		weaponDamage: 1
+	},level.shields);
 	
 	placeObjects({
 		name: "Potion",
 		loyalty: "object",
 		icon: "assets/Potion.svg",
 		description: "Heal one damage. not able to carry right now"
-	},config.potions);
+	},level.potions);
 }
